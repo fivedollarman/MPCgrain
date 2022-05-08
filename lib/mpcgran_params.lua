b2c9814149bf0892e55a2f9d6056bca2f2318252
Marco Cinque <el5grana@gmail.com>
2022-05-08 23:09:26 +0200

    Create mpcgran_params.lua

---
+++ lib/mpcgran_params.lua
@@ -0,0 +1,93 @@
+local MPCgrain = {}
+local Formatters = require 'formatters'
+
+-- ranges
+local specs = {
+  ["pos"] = controlspec.new(1, 8, "lin", 1, 1, ""),
+  ["bpm"] = controlspec.new(0, 240, "lin", 1, 120, ""),
+  ["step"] = controlspec.new(1, 128, "lin", 1, 1, ""),
+  ["amp"] = controlspec.new(0, 1, 'lin', 0, 1, ""),
+  ["att"] = controlspec.new(0, 4, "lin", 0.05, 1, "s"),
+  ["rel"] = controlspec.new(0, 8, "lin", 0.1, 2, "s"),
+  ["rnode"] = controlspec.new(0, 2, "lin", 1, 1, ""),
+  ["trgsel"] = controlspec.new(0, 1, "lin", 1, 0, ""),
+  ["trgfrq"] = controlspec.new(0, 128, "lin", 1, 8, ""),
+  ["rate"] = controlspec.new(0, 1, "lin", 0, 1, ""),
+  ["dur"] = controlspec.new(0, 2, "lin", 0.02, 1, ""),
+  ["transp"] = controlspec.new(-36, 36, "lin", 0, 0, ""),
+  ["filtcut"] = controlspec.new(12, 139, "lin", 1, 127, ""),
+  ["rq"] = controlspec.new(1, 0, "lin", 0, 0, ""),
+  ["delr"] = controlspec.new(0, 127, "lin", 1, 0, "ms"),
+  ["dell"] = controlspec.new(0, 127, "lin", 1, 0, "ms"),
+  ["drywet"] = controlspec.new(0, 1, "lin", 0, 0, ""),
+  ["pan"] = controlspec.PAN
+}
+
+local param_names = {"pos", "bpm", "step", "amp", "att", "rel", "rnode", "trgsel", "trgfrq", "rate", "dur", "transp", "filtcut", "rq", "delr", "dell", "drywet", "pan"}
+
+local mspecs = {
+  ["mpos"] = controlspec.new(1, 8, "lin", 1, 1, ""),
+  ["mbpm"] = controlspec.new(0, 240, "lin", 1, 120, ""),
+  ["mstep"] = controlspec.new(1, 128, "lin", 1, 1, ""),
+  ["mamp"] = controlspec.new(0, 1, 'lin', 0, 1, ""),
+  ["matt"] = controlspec.new(0, 4, "lin", 0.05, 1, "s"),
+  ["mrel"] = controlspec.new(0, 8, "lin", 0.1, 2, "s"),
+  ["mrnode"] = controlspec.new(0, 2, "lin", 1, 1, ""),
+  ["lfof"] = controlspec.new(0, 4, "lin", 0, 0, ""),
+  ["lfoph"] = controlspec.new(0, 3.14, "lin", 0, 0, ""),
+  ["lfofq"] = controlspec.new(0, 1, "exp", 0, 0, ""),
+  ["noiseq"] = controlspec.new(0, 2, "exp", 0, 0, ""),
+  ["mfiltcut"] = controlspec.new(1, 128, "lin", 1, 1, ""),
+  ["mastermod"] = controlspec.new(0, 1, "lin", 0, 1, ""),
+  ["pitchmod"] = controlspec.new(0, 1, "lin", 0, 1, ""),
+  ["durmod"] = controlspec.new(0, 1, "lin", 0, 1, ""),
+  ["trigftmod"] = controlspec.new(0, 1, "lin", 0, 1, ""),
+  ["posmod"] = controlspec.new(0, 1, "lin", 0, 1, ""),
+  ["filtmod"] = controlspec.new(0, 1, "lin", 0, 1, ""),
+  ["panmod"] = controlspec.new(0, 1, "lin", 0, 1, ""),
+  ["dellmod"] = controlspec.new(0, 1, "lin", 0, 1, ""),
+  ["delrmod"] = controlspec.new(0, 1, "lin", 0, 1, ""),
+}
+
+local mparam_names = {"mpos", "mbpm", "mstep", "mamp", "matt", "mrel", "mrnode", "lfof", "lfoph", "lfoq", "noiseq", "mfiltcut", "mastermod", "pitchmod", "durmod", "trigfmod", "posmod", "filtmod", "panmod", "dellmod", "delrmod"}
+
+-- initialize parameters:
+function MPCgrain.add_params()
+  params:add_group("MPCgrain", #param_names+1)
+  params:add_separator("prog")
+  for i = 1,#param_names do
+    local p_name = param_names[i]
+    params:add{
+      type = "control",
+      id = "MPCgrain_"..p_name,
+      name = p_name,
+      controlspec = specs[p_name],
+      formatter = p_name == "pan" and Formatters.bipolar_as_pan_widget or nil,
+      action = function(x) engine[p_name](x) end
+    }
+  end
+  params:add_group("MPCgrainmod", #mparam_names+1)
+  params:add_separator("modulation")
+  for i = 1,#mparam_names do
+    local mp_name = mparam_names[i]
+    params:add{
+      type = "control",
+      id = "MPCgrain_"..mp_name,
+      name = mp_name,
+      controlspec = mspecs[mp_name],
+      action = function(x) engine[mp_name](x) end
+    }
+  end
+  
+  params:bang()
+end
+
+-- a single-purpose triggering command fire a note
+function MPCgrain.trig(gate)
+  if gate ~= nil then
+    engine.gate(gate)
+  end
+end
+
+ -- we return these engine-specific Lua functions back to the host script:
+return MPCgrain
