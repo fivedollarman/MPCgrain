cda8762aaba43723b36c6b0d6839eb1e001b508e
Marco Cinque <el5grana@gmail.com>
2022-05-26 22:25:12 +0200

    Update mpcgran_params.lua

--- lib/mpcgran_params.lua
+++ lib/mpcgran_params.lua
@@ -3,7 +3,7 @@ local Formatters = require 'formatters'
 
 -- ranges
 local specs = {
-  ["step"] = controlspec.new(1, 128, "lin", 1, 1, ""),
+  ["step"] = controlspec.new(1, 16, "lin", 1, 1, ""),
   ["amp"] = controlspec.new(0, 1, 'lin', 0, 1, ""),
   ["att"] = controlspec.new(0, 4, "lin", 0.05, 0, "s"),
   ["rel"] = controlspec.new(0, 8, "lin", 0.1, 1, "s"),
@@ -60,11 +60,10 @@ local rspecs = {
   ["rpos"] = controlspec.new(1, 8, "lin", 1, 1, ""),
   ["rlvl"] = controlspec.new(0, 1, 'lin', 0, 1, ""),
   ["plvl"] = controlspec.new(0, 4, "lin", 0.05, 1, "s"),
-  ["loop"] = controlspec.new(0, 1, "lin", 1, 0, ""),
-  ["mode"] = controlspec.new(0, 1, "lin", 1, 0, "")
+  ["loop"] = controlspec.new(0, 1, "lin", 1, 0, "")
 }
 
-local rparam_names = {"rpos", "rlvl", "plvl", "loop", "mode"}
+local rparam_names = {"rpos", "rlvl", "plvl", "loop"}
 
 -- initialize parameters:
 function MPCgrain.add_params()
